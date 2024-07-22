dda90483b830187165fa5ca9ad8668a72c478d21
Giovanni Cappelletti <tech@methodwolf.com>
2024-07-22 14:33:03 +0200

    init

---
+++ src/main/java/org/java/spring_crud6/SpringCrud6Application.java
@@ -0,0 +1,13 @@
+package org.java.spring_crud6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrud6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrud6Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/java/spring_crud6/SpringCrud6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.spring_crud6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrud6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

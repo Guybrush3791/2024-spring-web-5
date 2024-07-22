d96d47828a0c906c8f0ef91ade3200fba56c7d55
Giovanni Cappelletti <tech@methodwolf.com>
2024-07-22 14:40:07 +0200

    update

--- src/main/java/org/java/spring_crud6/SpringCrud6Application.java
+++ src/main/java/org/java/spring_crud6/SpringCrud6Application.java
@@ -6,6 +6,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCrud6Application {
 
+	/**
+	 * TODO: CRUD PER PARTI
+	 * 
+	 * FASE 1: DB
+	 * Definire entita', repository e service
+	 * per le seguenti entita':
+	 * 
+	 * Bookshelf
+	 * - name : String
+	 * - address : String
+	 * 
+	 * Book
+	 * - title : String
+	 * - author : Autore
+	 * 
+	 * Author
+	 * - name : String
+	 * - surname : String
+	 * 
+	 * Definire inoltre le realzioni tra le entita' e
+	 * verificare in DbEaver se sono state create correttamente
+	 * 
+	 * --- CORREZIONE: 15:30 ---
+	 * 
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCrud6Application.class, args);
 	}

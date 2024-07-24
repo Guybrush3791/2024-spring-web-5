a52886653bfa8ecc1744d9e6b91dd921172b525e
Giovanni Cappelletti <tech@methodwolf.com>
2024-07-24 10:09:02 +0200

    update

--- src/main/java/org/java/spring_crud6/db/pojo/Bookshelf.java
+++ src/main/java/org/java/spring_crud6/db/pojo/Bookshelf.java
@@ -31,7 +31,7 @@ public class Bookshelf {
     public Bookshelf() {
     }
 
-    public Bookshelf(String name, String address, List<Book> books) {
+    public Bookshelf(String name, String address, List<Book> books) throws Exception {
 
         setName(name);
         setAddress(address);
@@ -50,7 +50,13 @@ public class Bookshelf {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) throws Exception {
+
+        if (name == null || name.isEmpty())
+            throw new Exception("Name is empty");
+
+        if (name.length() > 64)
+            throw new Exception("Name is too long");
 
         this.name = name;
     }
@@ -59,7 +65,13 @@ public class Bookshelf {
         return address;
     }
 
-    public void setAddress(String address) {
+    public void setAddress(String address) throws Exception {
+
+        if (name == null || name.isEmpty())
+            throw new Exception("Address is empty");
+
+        if (name.length() > 128)
+            throw new Exception("Address is too long");
 
         this.address = address;
     }
